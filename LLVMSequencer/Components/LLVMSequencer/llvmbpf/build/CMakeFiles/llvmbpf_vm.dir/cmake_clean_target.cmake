6708c2d3436d8f6da2d9ef5b471aa656baa221b4
Haoda Wang <harry@h313.info>
2025-04-29 13:50:21 -0400

    Remove old llvmbpf raw dirs

--- LLVMSequencer/Components/LLVMSequencer/llvmbpf/build/CMakeFiles/llvmbpf_vm.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/rpendergrast/llvmSeq/LLVMSequencer/lib/fprime/Svc/FpySequencer/llvmbpf/libllvmbpf_vm.a"
-)
--- LLVMSequencer/Components/LLVMSequencer/llvmbpf/build/example/CMakeFiles/maps-example.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for maps-example.
-# This may be replaced when dependencies are built.
