944736c391becc1a073e68283cfca56b921d986b
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2025-08-13 13:25:43 -0400

    Add test cases for BPF map (#4)
    * Implement BPF map tests and BPF shim
    
    * Add map lookup test
    
    * Rename to BPF-Prime ;)
    
    * Update README.md
    
    * snl1 is down so I can't get to the actual riscv.cmake smhhhh
    
    * Update to FPrime 4.0 and rename test deployment
    
    * Move toolchain location
    
    * Implement BPF map tests and BPF shim
    
    * Add map lookup test
    
    ---------
    
    Co-authored-by: Harry <haoda.wang@columbia.edu>
    Co-authored-by: Haoda Wang <harry@h313.info>

---
+++ tests/arraymap/program.bpf.c
@@ -0,0 +1,17 @@
+#include "../bpf_shim.h"
+
+int main() {
+    void *map = MAP_BY_FD(0);
+    int key = 0;
+    int value = 0xDEADBEEF;
+
+    bpf_map_update_elem(map, &key, &value, 0);
+
+    map = MAP_BY_FD(1);
+    key = 2;
+    value = 0xABADCAFE;
+
+    bpf_map_update_elem(map, &key, &value, 0);
+
+    return 0;
+}
