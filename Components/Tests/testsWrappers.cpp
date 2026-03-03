4cc7289002d1d2bcc228458de039964542314f79
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2026-03-03 15:30:57 -0500

    Improve populate_map_random

--- Components/Tests/testsWrappers.cpp
+++ Components/Tests/testsWrappers.cpp
@@ -256,35 +256,33 @@ Fw::Success Tests::benchmark() {
 Fw::Success Tests::populate_map_random(U32 fd, U32 start, U32 length) {
     auto map = reinterpret_cast<Components::map*>(maps::map_by_fd(fd));
     if (!map) {
-        Fw::LogStringArg errMsg(("Could not find map with fd " + std::to_string(fd)).c_str());
-        this->log_WARNING_LO_FailedToPopulateMap(errMsg);
+        auto errMsg = "Could not find map with fd " + std::to_string(fd);
+        this->log_WARNING_LO_FailedToPopulateMap(Fw::LogStringArg(errMsg.c_str()));
         return Fw::Success::FAILURE;
     }
 
     const auto max_key_size = sizeof(size_t);
     if (map->key_size > max_key_size) {
-        Fw::LogStringArg errMsg(
-            ("Map keys may not be greater than " + std::to_string(max_key_size) + " bytes").c_str());
-        this->log_WARNING_LO_FailedToPopulateMap(errMsg);
-        return Fw::Success::FAILURE;
-    }
-    if (map->max_entries < start + length) {
-        Fw::LogStringArg errMsg("Requested range exceeds map bounds");
-        this->log_WARNING_LO_FailedToPopulateMap(errMsg);
+        auto errMsg = "Map key size too large";
+        this->log_WARNING_LO_FailedToPopulateMap(Fw::LogStringArg(errMsg));
         return Fw::Success::FAILURE;
     }
 
-    const size_t totalBytes = map->value_size * length;
-    U8 values[totalBytes];
+    if (start >= map->max_entries || length > map->max_entries - start) {
+        auto errMsg = "Requested range exceeds map bounds";
+        this->log_WARNING_LO_FailedToPopulateMap(Fw::LogStringArg(errMsg));
+        return Fw::Success::FAILURE;
+    }
+    
+    std::vector<U8> value(map->value_size);
+    
+    static thread_local
+        std::independent_bits_engine<std::mt19937, CHAR_BIT, U8> engine(std::random_device{}());
 
-    std::independent_bits_engine<std::mt19937, CHAR_BIT, U8> engine(std::random_device{}());
-
-    std::generate(values, values + totalBytes, std::ref(engine));
-
-    for (size_t i = 0; i < length; ++i) {
-        size_t key = start + i;
-        U8* value = &values[i * map->value_size];
-        map->update_elem(&key, value, 0);
+    for (U32 i = 0; i < length; i++) {
+        U32 key = start + i;
+        std::generate(value.begin(), value.end(), std::ref(engine));
+        map->update_elem(&key, value.data(), 0);
     }
 
     return Fw::Success::SUCCESS;
